4422fcf78aea383585cd812242b62d23ea9ea6c4
Silviu-Munteanu <79187810+Silviu-Munteanu@users.noreply.github.com>
2022-10-07 13:10:55 +0300

    Add files via upload

--- Lab2/lab2ex1.py
+++ Lab2/lab2ex1.py
@@ -1,11 +1,18 @@
 import numpy
+import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 import math
 import random
 import arviz as az
+time=[]
 time1= stats.expon.rvs(0,1/4,size=10000)
 time2= stats.expon.rvs(0,1/6,size=10000)
-time=time1 * 0.4 + time2 * 0.6
+print(len(time1))
+for i in range(10000):
+    if random.random()<0.4:
+        time.append(time1[i])
+    else:
+        time.append(time2[i])
 az.plot_posterior({'M1':time1,'M2':time2,'AVG':time})
 plt.show()
