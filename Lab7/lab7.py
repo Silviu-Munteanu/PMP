77db7ded88a8cebcea32df1d69e19dd52cc5c69b
Silviu-Munteanu <79187810+Silviu-Munteanu@users.noreply.github.com>
2022-11-22 01:42:02 +0200

    Update lab7.py

--- Lab7/lab7.py
+++ Lab7/lab7.py
@@ -12,17 +12,39 @@ if __name__ == '__main__':
      HardDrive = data['HardDrive'].values
      Ram=data['Ram'].values
      Premium=data['Premium'].values
-     model=pm.Model()
-     #ex1
+#1
      with pm.Model() as model:
-          alpha=pm.Normal('alpha',mu=0,sd=1)
-          beta1=pm.Normal('beta1',mu=0,sd=1)
-          beta2=pm.Normal('beta2',mu=0,sd=1)
-          sigma=pm.HalfNormal('sigma',sd=1)
-          mu = pm.Deterministic('mu', alpha + beta1 * Speed + beta2 * [math.log(i) for i in HardDrive])
-          Price_obs=pm.Normal('Price_obs', mu=mu,sd=sigma,observed=Price)
-          trace = pm.sample(10, tune=10,return_inferencedata=True)
-     az.plot_posterior(trace)
-     #ex2
-     #az.plot_posterior({"beta1": trace['beta1'], "beta2": trace['beta2']},hdi_prob=0.95)
+          alpha = pm.Normal('alpha', mu=0, sd=1)
+          beta1 = pm.Normal('beta1', mu=0, sd=1)
+          beta2 = pm.Normal('beta2', mu=0, sd=1)
+          sigma = pm.HalfNormal('sigma', sd=1)
+          mu=pm.Deterministic('mu', alpha + beta1 * Speed + beta2 * [math.log(i) for i in HardDrive])
+          Price_obs = pm.Normal('price_obs', mu=mu, sigma=sigma, observed=Price)
+          trace = pm.sample(5000,tune=1000)
+          predictive = pm.sample_posterior_predictive(trace, var_names=["mu"], samples=5000)
+#2
+     az.plot_posterior(
+          { "beta1": trace['beta1'], "beta2": trace['beta2']},
+          hdi_prob=0.95)
+     plt.savefig("beta1_beta2.png")
+#4
+     az.plot_posterior(
+           {"pret asteptat": trace['mu']},
+           hdi_prob=0.9)
+#5
+     #az.plot_posterior({"pret prezis": predictive},hdi_prob=0.9) nu merge :(
+#3
+#Din beta1_beta2.png, avand in vedere ca in medie beta1 si beta 2 sunt !=0 si au o deviatie standard acceptabila sunt indicatori utili ai pretului.
+#bonus
+     with pm.Model() as model:
+          alpha = pm.Normal('alpha', mu=0, sd=1)
+          beta = pm.Normal('beta', mu=0, sd=1)
+          sigma = pm.HalfNormal('sigma', sd=1)
+          mu=pm.Deterministic('mu',alpha + beta * [0 if i=="no" else 1 for i in Premium])
+          Price_obs = pm.Normal('price_obs', mu=mu, sigma=sigma, observed=Price)
+          trace = pm.sample(5000, tune=1000)
+     az.plot_posterior(
+          {"premium_beta" : trace['beta']})
+     plt.savefig("premium.png")
      plt.show()
+#Din premium.png, avand in vedere ca media beta este aproape 0, faptul ca producatorul este premium are un impact prea mare
